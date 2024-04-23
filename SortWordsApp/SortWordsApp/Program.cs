fdf5ef760d14a115dc2b6bd2a9e6a0a400cafc3b
michal benhos <michalbenhos@gmail.com>
2024-04-23 13:24:30 +0300

    before refactoring

--- SortWordsApp/SortWordsApp/Program.cs
+++ SortWordsApp/SortWordsApp/Program.cs
@@ -1,19 +1,35 @@
-﻿namespace SortWordsApp
+﻿using System.Text.RegularExpressions;
+
+namespace SortWordsApp
 {
     class Pogram
     {
-        static async Task Main()
+        static async Task Main(string[] args)
         {
-            string[] filePaths = {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Please provide at least three file paths as arguments.");
+            }
+            /*string[] filePaths = {
                 @"C:\Users\micha\source\repos\SortWordsApp\file1.txt",
                 @"C:\Users\micha\source\repos\SortWordsApp\file2.txt",
                 @"C:\Users\micha\source\repos\SortWordsApp\file3.txt"
-            };
+            };*/
+
+            string[] filePaths = args;
+            Console.WriteLine("Enter your sorting option (-a for asceding, -d for desceding):");
+            string sortOption = Console.ReadLine();
+
+            Console.WriteLine("Enter your spliting character (-s for space, -c for comma, -n for new line:");
+            string splitOption = Console.ReadLine();    
 
             try
             {
                 var fileContents = await ReadAllFilesAsync(filePaths);
-                DisplayContents(fileContents);
+                var (processedContent, mostFrequentWord, maxCount) = ProcessContents(fileContents, sortOption, splitOption);
+                await WriteToFileAsync(@"C:\Users\micha\source\repos\SortWordsApp\file4.txt", processedContent);
+                Console.WriteLine("Processed content written to file4.txt");
+                Console.WriteLine($"The most frequent word is '{mostFrequentWord}' with a count of {maxCount}.");
                
             }
             catch (Exception ex) 
@@ -21,6 +37,58 @@
                 Console.WriteLine("An error occurred: " + ex.Message);
             }
 
+        }
+
+        static (string, string, int) ProcessContents(string[] contents, string sortOption, string splitOption)
+        {
+            var combinedContent = string.Join(" ", contents).ToLower();
+            var normalized = Regex.Replace(combinedContent, @"[\p{P}]", "");
+            var words = normalized.Split(new[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var wordCount = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else 
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            var sortedWords = (sortOption == "-d" ? wordCount.Keys.OrderByDescending(word => word) : 
+                                                    words.Distinct().OrderBy(word => word)).ToList();
+
+            int maxCount = wordCount.Values.Max();
+            var mostFrequentWord = wordCount.FirstOrDefault(x => x.Value == maxCount).Key;
+            string delimiter = GetDelimiter(splitOption);
+
+            return (string.Join(delimiter, sortedWords), mostFrequentWord, maxCount);
+        }
+
+        static string GetDelimiter(string splitOption) 
+        {
+            switch (splitOption) 
+            {
+                case "-s":
+                    return " ";
+                case "-c":
+                    return ", ";
+                case "-n":
+                    return "\n";
+                default:
+                    return ", ";
+            }
+        }
+
+        static async Task WriteToFileAsync(string filePath, string content)
+        {
+            using (StreamWriter outputFile = new StreamWriter(filePath)) 
+            {
+                await outputFile.WriteAsync(content);
+            }
         }
 
         static async Task<string[]> ReadAllFilesAsync(string[] filePaths)
@@ -43,16 +111,6 @@
             }
 
             return await File.ReadAllTextAsync(path);
-        }
-
-        static void DisplayContents(string[] contents) 
-        {
-            foreach (var item in contents) 
-            {
-                Console.WriteLine("File content:");
-                Console.WriteLine(item);
-                Console.WriteLine("----------------------------------------------------");
-            }
         }
     }
 
